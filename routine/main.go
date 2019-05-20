187ef8e415e9de0612559c0840f68fe4ea844d2b
yongliu1992 <a3831524@126.com>
2019-05-21 07:22:18 +0800

    goroutine channel

---
+++ routine/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var done = make(chan bool)
+	var data = make(chan int)
+	go product(data)
+	go consumer(data, done)
+	<-done
+}
+
+func product(data chan int) {
+	for i := 0; i < 8; i++ {
+		data <- i
+	}
+	close(data)
+}
+
+func consumer(nums chan int, done chan<- bool) {
+	for x := range nums {
+		fmt.Println(x)
+	}
+	done <- true
+}
