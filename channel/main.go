6b893d098f6b5f8c862f69b480877f01e1be7b24
yongliu1992 <a3831524@126.com>
2019-05-22 15:01:34 +0800

    readme init

--- channel/main.go
+++ channel/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	var ch = make(chan string, 3)
@@ -18,4 +21,37 @@ func main() {
 	fmt.Println(<-ch)
 	fmt.Println(<-ch)
 	fmt.Println(<-ch)
+	close(ch)
+
+	for ch2 := range getIntChan() {
+		fmt.Println(ch2)
+	}
+
+	intChannels := [3]chan int{
+		make(chan int, 1),
+		make(chan int, 1),
+		make(chan int, 1),
+	}
+	index := rand.Intn(3)
+	intChannels[index] <- index
+	select {
+	case <-intChannels[0]:
+		fmt.Println("The first candidate case is selected.")
+	case <-intChannels[1]:
+		fmt.Println("The second candidate case is selected.")
+	case elem := <-intChannels[2]:
+		fmt.Printf("The third candidate case is selected, the element is %d.\n", elem)
+	default:
+		fmt.Println("No candidate case is selected!")
+	}
+}
+
+func getIntChan() <-chan int {
+	num := 5
+	ch := make(chan int, num)
+	for i := 0; i < num; i++ {
+		ch <- i
+	}
+	close(ch)
+	return ch
 }
